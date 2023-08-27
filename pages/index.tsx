871f997eb47d336856d8dbd45bae6ce4d22ea26e
Patrick Zhang <zhangpatrick2004@gmail.com>
2023-08-27 02:03:55 -0400

    mvp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,215 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter, Raleway } from 'next/font/google'
+import Head from 'next/head'
+import { useEffect, useRef, useState } from 'react';
+import { FaExpandArrowsAlt } from 'react-icons/fa';
+import { GrClose } from 'react-icons/gr';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import path from 'path';
+import { promises as fs } from 'fs';
+
+
+const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+type Student = {
+  name: string;
+  school_id: number
+}
+
+type School = {
+  id: number
+  name: string;
+  coords: [number, number]
+}
+
+const fadeIn = {
+  opacity: 1,
+  transition: {
+    duration: 0.5
+  }
+}
+
+const transparent = {
+  opacity: 0,
+}
+
+const fadeOut = {
+  opacity: 0,
+  transition: {
+    duration: 0.5
+  }
+}
 
 const inter = Inter({ subsets: ['latin'] })
+const raleway = Raleway({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ students, schools } : InferGetStaticPropsType<typeof getStaticProps>) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const map = useRef(null);
+  const mapInstance = useRef<any>(null);
+  mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+  useEffect(() => {
+    if (mapInstance.current) return;
+    mapInstance.current = new mapboxgl.Map({
+      container: map.current,
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [-92.032, 38],
+      zoom: 3.8,
+      attributionControl: false,
+      logoPosition: 'top-left'
+    }).addControl(new mapboxgl.AttributionControl({
+      // compact: true,
+      
+    }), 'bottom-left');
+
+    for (const school of schools) {
+      if (school.coords === null) continue;
+      console.log(school)
+      let studentList = students.filter((s) => s.school_id === school.id).map((s) => `<li>${s.name}</li>`).join('')
+      console.log(studentList)
+      let marker = new mapboxgl.Marker({ color: 'black' })
+        .setLngLat(school.coords)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<b>${school.name}</b><ul>${studentList}</ul>`))
+        .addTo(mapInstance.current);
+
+      marker.getElement().addEventListener('click', () => {
+        flyTo(school.coords);
+      })
+    }
+
+  }, [])
+
+  function flyTo(LngLat: [number, number]) {
+    if (!mapInstance.current) return;
+    setMenuOpen(false);
+    mapInstance.current.flyTo({
+      center: LngLat,
+      zoom: 13,
+      essential: true
+    })
+  }
+
+  function flyToReset() {
+    if (!mapInstance.current) return;
+    setMenuOpen(false);
+    mapInstance.current.flyTo({
+      center: [-92.032, 38],
+      zoom: 3.8,
+      essential: true
+    })
+  }
+
+  const menuContent = 
+        <>
+          {
+            schools.slice(1).map((school, i) => {
+              let _students = students.filter(student => student.school_id === school.id)
+              return (
+                <div className='flex flex-col p-1 pl-4' key={i}>
+                  <h2 className='font-bold text-lg max-w-[10em] cursor-pointer underline' onClick={() => flyTo(school.coords)}>{ school.name }</h2>
+                  { _students.map((student, j) => <p className=' pl-3'>{ student.name }</p> )}
+                </div>
+              )
+            })
+          }
+          <div className='flex flex-col p-1 pl-4' key={-1}>
+            <h2 className='font-bold text-lg max-w-[10em]'>Gap Year</h2>
+            { students.filter(student => student.school_id === 0).map((student, j) => <p className=' pl-3'>{ student.name }</p> )}
+          </div>
+
+          <br />
+        </>
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <Head>
+        <title>Blair Magnet Class of '23 Destinations</title>
+        {/* <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+          rel="stylesheet"
+        /> */}
+      </Head>
+      <div className="absolute left-0 top-0 w-full h-full" ref={map} />
+      <h1 className={`text-4xl font-bold z-0`}>Blair Magnet Class of '23 Destinations</h1>
+
+      {/* <motion.div className={`absolute bottom-0 right-0 py-4 ` 
+          + (menuOpen ? `h-screen w-full overflow-y-auto bg-white` 
+                      : `h-full w-60 overflow-y-scroll bg-menu`)} layout="position">
+        <div className={`${raleway.className} flex ${menuOpen ? 'flex-wrap h-[95vh] flex-col pt-[5vh]' : 'flex-col'}`}>
+          { 
+            menuContent
+          }
         </div>
-      </div>
+      </motion.div> */}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <AnimatePresence>
+        {
+          menuOpen ?
+          <motion.div className="absolute bottom-0 right-0 py-4 h-screen w-full overflow-auto bg-white" key="fullscreen"
+            // initial={transparent}
+            // animate={fadeIn}
+            // exit={fadeOut}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className={`${raleway.className} flex flex-wrap h-[95vh] flex-col pt-[5vh]`}>
+              { 
+                menuContent
+              }
+            </div>
+          </motion.div>
+          :
+          <motion.div className="absolute bottom-0 right-0 py-4 h-full w-60 overflow-y-scroll bg-menu" key="sidebar"
+            // initial={transparent}
+            // animate={fadeIn}
+            // exit={fadeOut}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className={`${raleway.className} flex flex-col`}>
+              { 
+                menuContent
+              }
+            </div>
+          </motion.div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        }
+      </AnimatePresence>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {
+        menuOpen ? 
+        <div className="absolute right-0 top-2 aspect-square w-12 bg-menu rounded-l-lg flex items-center justify-center">
+          <GrClose className="text-2xl text-black cursor-pointer" onClick={() => setMenuOpen(false)}/>
+        </div>
+        :
+        <>
+        <div className="absolute right-60 top-2 aspect-square w-12 bg-menu rounded-l-lg flex items-center justify-center">
+          <FaExpandArrowsAlt className="text-2xl text-black cursor-pointer" onClick={() => setMenuOpen(true)}/>
+        </div>
+        <p className='absolute bottom-0 right-60 mr-2 text-red-400 cursor-pointer' onClick={flyToReset}>reset</p>
+        </>
+      }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
+}
+
+export const getStaticProps: GetStaticProps<{ students: Student[], schools: School[] }> = async () => {
+  const students = await fs.readFile(path.join(process.cwd(), 'data', 'students.json'), 'utf-8');
+  const schools = await fs.readFile(path.join(process.cwd(), 'data', 'schools.json'), 'utf-8');
+
+  return {
+    props: {
+      students: JSON.parse(students),
+      schools: JSON.parse(schools)
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        'menu': 'rgba(255,255,255,0.9)'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      gridTemplateRows: {
+        'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+        'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+        '2em': 'repeat(auto-fill, 1em)',
+      }
     },
   },
   plugins: [],
